c3e56af359728c2874d872666a7d0ec2e0e4ec45
Johnnybar <johnnybareket@gmail.com>
2022-01-18 15:50:07 +0100

    pdf-extraction/UI-adds handleChange to upload pdf and retrieve info

--- src/UploadUi.tsx
+++ src/UploadUi.tsx
@@ -4,13 +4,27 @@ import OutputText from "./OutputText";
 pdfjsLib.GlobalWorkerOptions.workerSrc = "/js/pdf.worker.min.js";
 
 const UploadUi = () => {
+  const [isSelected, setIsSelected] = useState<Boolean>(false);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [extractedText, setExtractedText] = useState<Optional<string>>();
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    console.log(event.target.files[0]);
+
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
   return (
     <div>
-      <input type="file" name="file" />
-      <p>Select a file to show details</p>
-
+      <input type="file" name="file" onChange={handleChange} />
+      {isSelected && selectedFile ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>{" "}
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
       <div>
         <button>Submit</button>
       </div>
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,3 @@
 declare module "pdfjs-dist/build/pdf.js";
 type Optional<T> = T | null;
+type handleChange = (e: React.ChangeEvent<any>) => void;
