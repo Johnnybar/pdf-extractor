2bfa938115dc074258d64ce5031e1b5956bb600e
Johnnybar <johnnybareket@gmail.com>
2022-01-18 17:35:23 +0100

    pdf-extraction/functionality-adds upload, clear and copy functionality to UI, bootstrap classes

--- src/UploadUi.tsx
+++ src/UploadUi.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import * as pdfjsLib from "pdfjs-dist/build/pdf.js";
 import OutputText from "./OutputText";
-pdfjsLib.GlobalWorkerOptions.workerSrc = "/js/pdf.worker.min.js";
+import getText from "./getText";
 
 const UploadUi = () => {
   const [isSelected, setIsSelected] = useState<Boolean>(false);
@@ -9,32 +8,91 @@ const UploadUi = () => {
   const [extractedText, setExtractedText] = useState<Optional<string>>();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //to prevent typescript null value error
     if (!event.target.files) return;
-    console.log(event.target.files[0]);
 
     setSelectedFile(event.target.files[0]);
     setIsSelected(true);
+  };
+
+  const handleSubmission = () => {
+    getText(selectedFile).then(
+      (text: string) => {
+        setExtractedText(text);
+      },
+      (error: string) => {
+        console.error(error);
+      }
+    );
+  };
+
+  const clearText = () => {
+    setIsSelected(false);
+    setExtractedText(null);
+  };
+
+  const copyToClipboard = () => {
+    if (extractedText) {
+      navigator.clipboard
+        .writeText(extractedText)
+        .then(() => {
+          alert("successfully copied");
+        })
+        .catch(() => {
+          alert("something went wrong");
+        });
+    }
   };
   return (
     <div className="upload-ui container">
       <div className="row">
         <div className="col-sm-12 text-center">
           <h1 className="upload-ui__top-heading">PDF Extractor</h1>
+          <label className="custom-input">
+            <input type="file" name="file" onChange={handleChange} />
+            Upload PDF to Extract Text
+          </label>
 
-          <input type="file" name="file" onChange={handleChange} />
           {isSelected && selectedFile ? (
             <div>
               <p>Filename: {selectedFile.name}</p>{" "}
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
             </div>
           ) : (
             <p>Select a file to show details</p>
           )}
           <div>
-            <button>Submit</button>
+            <button
+              className="btn btn-primary upload-ui__utility-btn"
+              onClick={handleSubmission}
+            >
+              Submit
+            </button>
           </div>
-        </div>
-        <div className="upload-ui__list">
-          {extractedText && <OutputText output={extractedText} />}
+          <div className="upload-ui__list">
+            {extractedText && (
+              <div>
+                <OutputText output={extractedText} />
+                <div className="upload-ui__utility-btn">
+                  <button
+                    className="btn btn-outline-secondary"
+                    onClick={copyToClipboard}
+                  >
+                    Copy to Clipboard
+                  </button>
+                </div>
+                <div className="upload-ui__utility-btn">
+                  <button
+                    className="btn btn-secondary btn-block"
+                    onClick={clearText}
+                  >
+                    Clear
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
