d01795e23760aa4bf2d9f5184d558ded42eeed5d
Johnnybar <johnnybareket@gmail.com>
2022-01-18 17:36:15 +0100

    pdf-extraction/functionality-adds types for functions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,7 @@
 declare module "pdfjs-dist/build/pdf.js";
 type Optional<T> = T | null;
 type handleChange = (e: React.ChangeEvent<any>) => void;
+type handleSubmission = () => string;
+type clearText = () => void;
+type copyToClipboard = () => void;
+type getText = (file: File) => string;
